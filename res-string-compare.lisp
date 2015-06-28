abc9391acf8497944409415c271519c03bb2231c
leon <yjg.2016@gmail.com>
2015-06-29 02:02:33 +0800

    add hash-table to Performance promotion

--- res-string-compare.lisp
+++ res-string-compare.lisp
@@ -1,48 +1,86 @@
-;;;; 由于说原生对小语种支持缺失，如：values/strings.xml 有 <string name="a">a</string>，而values-fi/strings.xml 没有此字段。
+;;;; 由于说高通原生对小语种支持缺失，如：values/strings.xml 有 <string name="a">a</string>，而values-fi/strings.xml 没有此字段。
 ;;;; 此代码寻找出缺失的字符串
 
 (defparameter *base-strings* "values")
 (defparameter *compare-strings-array*
   (list "values-ca" "values-ca-rES" "values-cs" "values-cs-rCZ" "values-da" "values-da-rDK" "values-de" "values-de-rAT" "values-de-rch" "values-de-rDE" "values-el" "values-el-rGR" "values-en-rGB" "values-es" "values-es-rES" "values-fi" "values-fi-rFI" "values-fr" "values-fr-rBE" "values-fr-rCH" "values-fr-rFR" "values-hu" "values-hu-rHU" "values-it" "values-it-rIT" "values-nb" "values-nb-rNO" "values-nl-rBE" "values-nl-rNL" "values-pl" "values-pl-rPL" "values-pt" "values-pt-rBR" "values-pt-rPT" "values-ro" "values-ro-rRO" "values-ru" "values-ru-rRU" "values-sv" "values-sv-rSE" "values-tr" "values-tr-rTR" "values-zh-rCN" "values-zh-rHK" "values-zh-rTW"))
 
+(defparameter *source-table* (make-hash-table))
+(defparameter *compare-table* (make-hash-table :test 'equal))
+
+(defstruct tag content line-number)
+
+(defun base-xml-to-hash-table (dir-res)
+  (with-open-file (in (rtn-strings-xml dir-res *base-strings*)
+		      :direction :input
+		      :if-does-not-exist nil)
+    (let ((multi-line-p)
+	  (name)
+	  (multi-line-buffer))
+      (do ((line (read-line in nil 'eof)
+		 (read-line in nil 'eof))
+	   (line-number 1 (1+ line-number)))
+	  ((eql line 'eof))
+	(let ((name-position (search "name=" line)))
+	  (cond (name-position
+		 (setf name
+		       (subseq line
+			       (+ name-position 6)
+			       (search "\"" line :start2 (+ name-position 6))))
+		 (setf multi-line-p (not (search "</string>" line :from-end t)))
+		 (if multi-line-p
+		     (setf multi-line-buffer line)
+		     (set-source-hash name line line-number)))
+		(multi-line-p
+		 (setf multi-line-buffer (concatenate 'string multi-line-buffer line))
+		 (setf multi-line-p (not (search "</string>" line :from-end t)))
+		 (unless multi-line-p
+		   (set-source-hash name multi-line-buffer line-number)))))))))
+
+
+(defun set-source-hash (name content line-number)
+  (setf (gethash name *source-table*)
+	(make-tag :content content
+		  :line-number line-number)))
+
+(defun compare-xml-to-hash-table (dir-res dir-value)
+  (clrhash *compare-table*)
+  (with-open-file (in (rtn-strings-xml dir-res dir-value)
+		      :direction :input
+		      :if-does-not-exist nil
+		      :external-format :utf-8)
+    (when in
+      (do ((line (read-line in nil 'eof)
+		 (read-line in nil 'eof)))
+	  ((eql line 'eof) t)
+	(let ((name-position (search "name=" line)))
+	  (if name-position
+	      (let ((name (subseq line
+				  (+ name-position 6)
+				  (search "\"" line :start2 (+ name-position 6)))))
+		(setf (gethash name *compare-table*) t))))))))
+    
+
+
 (defun main (dir-res)
+  (base-xml-to-hash-table dir-res)
   (with-open-file (out "~/compare-strings-result.txt"
 		       :direction :output
 		       :if-exists :supersede)
-    (with-open-file (in (rtn-strings-xml dir-res *base-strings*)
-			:direction :input
-			:if-does-not-exist nil)
-      (when in
-	(dolist (dir-value *compare-strings-array*)
-	  (file-position in 0)
-	  (with-open-file (in2 (rtn-strings-xml dir-res dir-value)
-			       :direction :input
-			       :if-does-not-exist nil)
-	    (format out "~%~%~A~%" dir-value)
-	    (if in2
-		(compare-strings in in2 out)
-		(format out "file ~A is not exist~%" dir-value))))))))
+    (dolist (dir-value *compare-strings-array*)
+      (format out "~%~%~A~%" dir-value)
+      (if (compare-xml-to-hash-table dir-res dir-value)
+	  (compare-strings out)
+	  (format out "file ~A is not exist~%" dir-value))))
+  (clrhash *source-table*)
+  (clrhash *compare-table*))
 
 
-(defun compare-strings (in in2 out)
-  (do ((line (read-line in nil 'eof)
-	     (read-line in nil 'eof))
-       (line-number 1 (1+ line-number)))
-      ((eql line 'eof))
-    (file-position in2 0)
-    (let ((compare-line-p (search "name=" line)))
-      (if compare-line-p
-	  (let ((name (subseq line
-			      compare-line-p
-			      (search "\""
-				      line
-				      :start2 (+ compare-line-p 6)))))
-	    (do ((line2 (read-line in2 nil 'eof)
-			(read-line in2 nil 'eof)))
-		((eql line2 'eof)
-		 (format out "~A: ~A~%" line-number line))
-	      (if (search name line2)
-		  (return))))))))
+(defun compare-strings (out)
+  (maphash #'(lambda (k v)
+	       (unless (gethash k *compare-table*)
+		 (format out "~A:~A~%" (tag-line-number v) (tag-content v))))
+	   *source-table*))
 
 
 (defun rtn-strings-xml (dir-res dir-values)
